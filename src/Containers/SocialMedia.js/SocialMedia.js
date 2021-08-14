23e09b563edfd7b9dfa0de87c20d604ab908f0c2
Aparajita-046 <aparajita046@gmail.com>
2021-08-14 13:47:28 +0530

    Initial commit

---
+++ src/Containers/SocialMedia.js/SocialMedia.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SocialMedia.css'
+
+function SocialMedia() {
+    return (<>
+    
+    </>
+    )
+}
+
+export default SocialMedia
---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SocialMediaHorizontal from '../SocialMedia.js/SocialMediaHorizontal/SocialMediaHorizontal'
+import './footer.css'
+
+function footer() {
+
+    return (
+        <>
+            
+        </>
+    )
+}
+export default footer
